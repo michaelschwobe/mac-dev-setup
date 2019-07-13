f4db9cc90f9d1c1f28a347e5f1eb51a24def5286
Michael Schwobe <mschwobe@gmail.com>
2019-07-13 10:54:34 -0500

    Added command for updating oh-my-zsh

--- scripts/optimizations.sh
+++ scripts/optimizations.sh
@@ -10,3 +10,11 @@ if has_command "brew"; then
   e_pending "Optimizing Homebrew"
   brew update && brew upgrade && brew doctor && brew cleanup
 fi
+
+if has_command "zsh"; then
+  if has_path ".oh-my-zsh"; then
+    e_pending "Upgrading oh-my-zsh"
+    upgrade_oh_my_zsh
+    test_path ".oh-my-zsh"
+  fi
+fi
