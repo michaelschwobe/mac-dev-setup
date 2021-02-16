d324ced926dfd851e0f8fa2980fffe68fef09a6f
Michael Schwobe <mschwobe@gmail.com>
2021-02-15 20:10:46 -0600

    Fix `omz: command not found`

--- scripts/optimizations.sh
+++ scripts/optimizations.sh
@@ -16,7 +16,8 @@ fi
 if has_command "zsh"; then
   if has_path ".oh-my-zsh"; then
     e_pending "Updating oh-my-zsh"
-    omz update
+    # omz update
+    $ZSH/tools/upgrade.sh
     test_path ".oh-my-zsh"
   fi
 fi
